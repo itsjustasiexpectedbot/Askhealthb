0169194da574ae8e5ed4f289776744c2f8f96619
itsjustasiexpectedbot <itsjustasiexpected@gmail.com>
2026-05-17 11:19:33 +0530

    Add files via upload

---
+++ ask_health.py
@@ -0,0 +1,12 @@
+print(" Jawab 'haa' , yaa 'naa' , mai de ")
+ask1 = input(" Pehle se thoda v behtar nhi ")
+if ask1 != "haa" and ask1 != "naa":
+	print(" Aapne galat jawab chuna ")
+	print(" ye program aapse krodhit hua ")
+	print(" Good Bye")
+elif ask1 == "haa":
+	print(" Aapki tabiyat aur v behtar hogi jld hi  ")
+	print(" Take Care Bye !")
+elif ask1 == "naa":
+	print(" Koi baat nhi aap rest kare")
+	print(" ye program aapki hamesha sahayata karega")
